209b55874837c1185c64a20bcf793559041e6c9a
matthewsrsampaio <matthews_venom@hotmail.com>
2024-02-28 13:34:33 -0300

    function added

--- teste/teste.js
+++ teste/teste.js
@@ -2,3 +2,7 @@ function testandoGit() {
     let teste = document.getElementById('msg')
     teste.innerText = 'Deu tudo certo!'
 }
+
+function qualquerCoisa() {
+    console.log('Qualquer coisa')
+}
